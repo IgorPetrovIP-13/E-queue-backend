93e1313da8e037cd133adb5afde80f3ce75c8857
IgorPetrovIP-13 <garrypet04@gmail.com>
2025-02-04 01:07:41 +0200

    -

--- src/app/core/auth/auth.controller.ts
+++ src/app/core/auth/auth.controller.ts
@@ -66,7 +66,7 @@ export class AuthController {
     res.cookie(TokensEnum.REFRESH_TOKEN, refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
   }
